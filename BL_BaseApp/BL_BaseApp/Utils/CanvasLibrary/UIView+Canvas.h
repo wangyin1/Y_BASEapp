4de823c7fc829634fa238433d6e24603e85e130d
wangyin1 <w_hh528@yeah.net>
2016-12-09 17:49:15 +0800

    优化

---
+++ BL_BaseApp/BL_BaseApp/Utils/CanvasLibrary/UIView+Canvas.h
@@ -0,0 +1,26 @@
+//
+//  UIView+Canvas.h
+//  BL_BaseApp
+//
+//  Created by bolaa on 16/12/9.
+//  Copyright © 2016年 王印. All rights reserved.
+//
+#import "Aspects.h"
+#import <UIKit/UIKit.h>
+
+@interface UIView (Canvas)
+
+@property(nonatomic,strong)CSAnimationType      canvasType;//类型
+@property(nonatomic,assign)CGFloat              canvasDuration;//持续时间
+@property(nonatomic,assign)CGFloat              canvasDelay;//延时
+@property(nonatomic,assign)BOOL                 initStart;//是否初始化的时候触发一次
+
+//注册动画类型，默认初始化的时候不会触发
++ (void)registerAnimationType:(CSAnimationType)animationType;
+
+//触发动画
+- (void)startCanvasAnimation;
+
+//触发所有子视图动画／只遍历一层结构
+- (void)startSubviewCamvasAnimation;
+@end
---
+++ BL_BaseApp/BL_BaseApp/Utils/CanvasLibrary/UIView+Canvas.m
@@ -0,0 +1,71 @@
+//
+//  UIView+Canvas.m
+//  BL_BaseApp
+//
+//  Created by bolaa on 16/12/9.
+//  Copyright © 2016年 王印. All rights reserved.
+//
+
+#import "CSAnimation.h"
+#import "UIView+Canvas.h"
+
+
+
+@implementation UIView (Canvas)
+
+
+- (void)startSubviewCamvasAnimation{
+    [[self subviews] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [obj startCanvasAnimation];
+    }];
+}
+
+- (void)startCanvasAnimation{
+    
+    Class <CSAnimation> class = [CSAnimation classForAnimationType:self.canvasType];
+    [class performAnimationOnView:self duration:self.canvasDuration delay:self.canvasDelay];
+}
+
++ (void)registerAnimationType:(CSAnimationType)animationType{
+    [CSAnimation registerClass:self forAnimationType:animationType];
+}
+
+#pragma mark getter setter
+- (void)setCanvasType:(CSAnimationType)canvasType{
+     objc_setAssociatedObject(self, @selector(canvasType), canvasType, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)setInitStart:(BOOL)initStart{
+    objc_setAssociatedObject(self, @selector(initStart), @(initStart), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    if (initStart) {
+        [self aspect_hookSelector:@selector(awakeFromNib) withOptions:AspectPositionAfter usingBlock:^(){
+            [self startCanvasAnimation];
+        }error:nil];
+    }
+}
+
+- (BOOL)initStart{
+    return [objc_getAssociatedObject(self, @selector(initStart)) boolValue];
+}
+
+- (CSAnimationType)canvasType{
+  return objc_getAssociatedObject(self, @selector(canvasType));
+}
+
+- (void)setCanvasDelay:(CGFloat)canvasDelay{
+    objc_setAssociatedObject(self, @selector(canvasDelay), @(canvasDelay), OBJC_ASSOCIATION_ASSIGN);
+
+}
+- (CGFloat)canvasDelay{
+    return [objc_getAssociatedObject(self, @selector(canvasDelay)) floatValue];
+}
+
+- (void)setCanvasDuration:(CGFloat)canvasDuration{
+    objc_setAssociatedObject(self, @selector(canvasDuration), @(canvasDuration), OBJC_ASSOCIATION_ASSIGN);
+
+}
+
+- (CGFloat)canvasDuration{
+    return [objc_getAssociatedObject(self, @selector(canvasDuration)) floatValue];
+}
+@end
